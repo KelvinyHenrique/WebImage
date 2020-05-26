50d1125ab69a8bd94137b76cc9efb25603249c04
Kelviny <kelvinyhenrique17@gmail.com>
2020-05-26 17:34:19 -0300

    Dashboard

--- assets/php/main.php
+++ assets/php/main.php
@@ -22,54 +22,69 @@
                     echo "Erro ao realizar upload";
                 }        
             }
-}
-        }
+              }
+                }
 
         function cortar($files, $post) {
             
-            $image = $files;
-            $NewImageName = Rand(7,10000).".png";
-            $destination = realpath('../../images').'/';
-            move_uploaded_file($image['image']['tmp_name'], $destination.$NewImageName);
-            $image = imagecreatefromjpeg($destination.$NewImageName);
-            $filename = $destination.$NewImageName;
-        
-           /* $thumb_width = 200;
-            $thumb_height = 150;*/
-            $thumb_width = $post['width'];
-            $thumb_height = $post['height'];
-        
-            $width = imagesx($image);
-            $height = imagesy($image);
-        
-            $original_aspect = $width / $height;
-            $thumb_aspect = $thumb_width / $thumb_height;
-        
-            if ( $original_aspect >= $thumb_aspect )
-            {
-               // If image is wider than thumbnail (in aspect ratio sense)
-               $new_height = $thumb_height;
-               $new_width = $width / ($height / $thumb_height);
-            }
-            else
-            {
-               // If the thumbnail is wider than the image
-               $new_width = $thumb_width;
-               $new_height = $height / ($width / $thumb_width);
-            }
-        
-            $thumb = imagecreatetruecolor( $thumb_width, $thumb_height );
-        
-            // Resize and crop
-            imagecopyresampled($thumb,
-                               $image,
-                               0 - ($new_width - $thumb_width) / 2, // Center the image horizontally
-                               0 - ($new_height - $thumb_height) / 2, // Center the image vertically
-                               0, 0,
-                               $new_width, $new_height,
-                               $width, $height);
-            imagejpeg($thumb, $filename, 80);
-            return $NewImageName;          
+            if( isset($_FILES['image']['name'])) {
+      
+                $total_files = count($_FILES['image']['name']);
+                    for($key = 0; $key < $total_files; $key++) {
+            
+                        if(isset($_FILES['image']['name'][$key]) && $_FILES['image']['size'][$key] >= 0) {
+                            
+                            $name = $_FILES['image']['name'][$key];
+                            $tmp_name = $_FILES['image']['tmp_name'][$key];
+            
+                            $NewImageName = Rand(7,10000).".png";
+                            $destination = realpath('../../images').'/';
+                            move_uploaded_file($tmp_name, $destination.$NewImageName);
+                            $image = imagecreatefromjpeg($destination.$NewImageName);
+                            $filename = $destination.$NewImageName;
+                        
+                            $thumb_width = 200;
+                            $thumb_height = 150;
+                        
+                            $width = imagesx($image);
+                            $height = imagesy($image);
+                        
+                            $original_aspect = $width / $height;
+                            $thumb_aspect = $thumb_width / $thumb_height;
+                        
+                            if ( $original_aspect >= $thumb_aspect )
+                            {
+                               // If image is wider than thumbnail (in aspect ratio sense)
+                               $new_height = $thumb_height;
+                               $new_width = $width / ($height / $thumb_height);
+                            }
+                            else
+                            {
+                               // If the thumbnail is wider than the image
+                               $new_width = $thumb_width;
+                               $new_height = $height / ($width / $thumb_width);
+                            }
+                        
+                            $thumb = imagecreatetruecolor( $thumb_width, $thumb_height );
+                        
+                            // Resize and crop
+                            imagecopyresampled($thumb,
+                                               $image,
+                                               0 - ($new_width - $thumb_width) / 2, // Center the image horizontally
+                                               0 - ($new_height - $thumb_height) / 2, // Center the image vertically
+                                               0, 0,
+                                               $new_width, $new_height,
+                                               $width, $height);
+                            imagejpeg($thumb, $filename, 80);
+                            echo "cropped";
+            
+                            
+            
+                        }
+                        
+                    }
+                        
+                    }          
         }
         
     }
